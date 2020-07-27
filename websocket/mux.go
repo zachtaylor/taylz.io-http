6e789d7fdafc35d6ad834ffc5cd7e0eb0f186a5d
Zach Taylor <zach@ztaylor.me>
2020-07-27 10:44:07 -0500

    update doc
    Signed-off-by: Zach Taylor <zach@ztaylor.me>

--- websocket/mux.go
+++ websocket/mux.go
@@ -3,17 +3,17 @@ package websocket
 // Mux is a slice of *Route that implements Handler
 type Mux []*Route
 
-// Append adds a WebsocketRoute to this Mux
+// Append adds a Route to this Mux
 func (mux *Mux) Append(r *Route) {
 	*mux = append(*mux, r)
 }
 
-// Route is a macro for Append(WebsocketRoute{})
+// Route is a macro for Append(Route{})
 func (mux *Mux) Route(r Router, h Handler) {
 	mux.Append(&Route{r, h})
 }
 
-// ServeWS satisfies WebsocketServer by routing to a matching *WebsocketRoute
+// ServeWS satisfies Handler by routing to a matching *Route
 func (mux *Mux) ServeWS(ws *T, m *Message) {
 	var handler Handler
 	for _, route := range *mux {
